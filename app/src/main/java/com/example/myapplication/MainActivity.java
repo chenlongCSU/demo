7635eba75222e17714bdcf7d87e0afd09d11f466
chenlong <2512119987@qq.com>
2021-01-02 18:26:29 +0800

    2021年的第一次修改

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        myLinear myLinear = findViewById(R.id.myLinear);
+//        leftLinear leftLinear = new leftLinear(MainActivity.this,null);
+//        leftLinear.addView(new myText(MainActivity.this));
+//        myLinear.addView(leftLinear);
+        Log.i("daronchen","1");
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Log.i("daronchen","2");
+            }
+        },4000);
+        Log.i("daronchen","3");
+        Log.i("daronchen","5");
+        Log.i("daronchen","6");
+        Log.i("daronchen","7");
     }
+
 }
